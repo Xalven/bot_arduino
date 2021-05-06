750e60a2a6211ed57312847e748fdba7efb8e397
RomainCuchet <47221720+RomainCuchet@users.noreply.github.com>
2021-05-06 17:21:46 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,31 +1,28 @@
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(12,11,5,4,3,2); //initialisation des broches de l'écran lcd
+const int ultrasound_sensor_trigger_pin = 2;
+const int ultrasound_sensor_echo_pin = 4;
+long lecture_echo; // long[2^31-1;2^31-1]
+long obstacle_distance;
+bool is_obstacle = false // true if there is an obstacle
 
-const int ultrasound_sensor_trigger_pin = 8;
-const int ultrasound_sensor_echo_pin = 7;
-float distance;
-bool state_ultrasoud_sensor = true;
-bool obstacle = false;
-
-float distance_mesurement(){
-    float distance;
-    digitalWrite(ultrasound_sensor_trigger_pin, HIGH);
-    delayMicroseconds(2);
-    digitalWrite(ultrasound_sensor_trigger_pin, LOW);
-    delayMicroseconds(10);
-    distance= pulseIn(ultrasound_sensor_echo_pin, HIGH) / 58.0;
-    return distance;
+long distance_mesurement(){
+  digitalWrite(ultrasound_sensor_trigger_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ultrasound_sensor_trigger_pin, LOW);
+  lecture_echo = pulseIn(ultrasound_sensor_echo_pin,HIGH);
+  obstacle_distance = lecture_echo /58;
+  return distance;
 }
 
+void setup(){
+  pinMode(ultrasound_sensor_trigger_pin, OUTPUT);
+  digitalWrite(ultrasound_sensor_trigger_pin, LOW);
+  pinMode(ultrasound_sensor_echo_pin, INPUT);
+  Serial.begin(9600);
+}
 
-void setup()
-  {
-    pinMode(ultrasound_sensor_trigger_pin, OUTPUT);
-    pinMode(ultrasound_sensor_echo_pin, INPUT);
-    Serial.begin(9600);
-    lcd.begin(16,2);
-  }
-void loop()
-  {
-    distance = distance_mesurement();
-  } 
+void loop(){
+  obstacle_distance = distance_mesurement();
+  Serial.print("Distance en cm :");
+  Serial.println(distance);
+  delay(1500);
+}
