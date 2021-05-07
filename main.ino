3305e8e7c8a9d2d0e1412828ee9f15647867455c
Xalven <59165351+Xalven@users.noreply.github.com>
2021-05-07 19:05:14 +0200

    Updating var, adding line follower
    I've also add turn function but as we don't have the motor S/O Ait Moussa I can't do it

--- main.ino
+++ main.ino
@@ -1,9 +1,12 @@
-const int ultrasound_sensor_trigger_pin = 2;
-const int ultrasound_sensor_echo_pin = 4;
-const int buzzer_pin = 7;
+const byte ultrasound_sensor_trigger_pin = 2;
+const byte ultrasound_sensor_echo_pin = 4;
+const byte infrared_sensor_right_pin = ;
+const byte infrared_sensor_left_pin = ;
 long lecture_echo; // long[2^31-1;2^31-1]
 long obstacle_distance;
 bool is_obstacle = false; // true if there is an obstacle
+bool infrared_sensor_left_state; // HIGH or LOW
+bool infrared_sensor_right_state; // HIGH or LOW
 
 long distance_mesurement(){
   digitalWrite(ultrasound_sensor_trigger_pin, HIGH);
@@ -14,22 +17,53 @@ long distance_mesurement(){
   return obstacle_distance;
 }
 
+void line_follower(){
+  infrared_sensor_right_state = digitalRead(infrared_sensor_right_pin)
+  infrared_sensor_left_state = digitalRead(infrared_sensor_left_pin)
+  if (infrared_sensor_right_state == LOW and infrared_sensor_left_state == LOW){
+    forward()
+  }
+  else if (infrared_sensor_right_state == HIGH and infrared_sensor_left_state == LOW){
+    turn_right(degree = 5)
+  }
+  else if (infrared_sensor_right_state == LOW and infrared_sensor_left_state == HIGH){
+    turn_left(degree = 5)
+  }
+  else if (infrared_sensor_right_state == HIGH and infrared_sensor_left_state == HIGH){
+    turn_(degree = 90)// à déterminer
+  }
+}
+void turn_left(degree){
+
+}
+void turn_right(degree){
+  
+}
+void forward(){
+  
+}
+void backward(){
+  
+}
+
 void Serialprint(long obstacle_distance){
   Serial.print("Distance en cm :");
   Serial.println(obstacle_distance);
   
 }
 
-
 void setup(){
   pinMode(ultrasound_sensor_trigger_pin, OUTPUT);
   digitalWrite(ultrasound_sensor_trigger_pin, LOW);
-  pinMode(ultrasound_sensor_echo_pin, INPUT)
+  pinMode(ultrasound_sensor_echo_pin, INPUT);
+  pinMode(infrared_sensor_left_pin, INPUT)
+  pinMode(infrared_sensor_right_pin, INPUT)
   Serial.begin(9600);
+
 }
 
 void loop(){
   obstacle_distance = distance_mesurement();
   Serialprint(obstacle_distance);
-  delay(1000);
+  delay(1500);
 }
